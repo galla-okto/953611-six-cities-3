ef216e5865058454054fe07e65dbe1780a0582de
galla-okto <galla.okto1@gmail.com>
2020-02-09 00:03:45 +0200

    Revert "Исправляет ошибки"
    This reverts commit c9353ee62f3a1a153bc6ebafe02f2ea060e00bdd.

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,45 +1,43 @@
 import React, {Fragment} from 'react';
 import types from './../types/types.js';
 
-const Offer = ({offer}) => {
-  return (
-    <Fragment>
-      <article className="cities__place-card place-card">
-        <div className="place-card__mark">
-          <span>Premium</span>
-        </div>
-        <div className="cities__image-wrapper place-card__image-wrapper">
-          <a href="#">
-            <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
-          </a>
-        </div>
-        <div className="place-card__info">
-          <div className="place-card__price-wrapper">
-            <div className="place-card__price">
-              <b className="place-card__price-value">&euro;120</b>
-              <span className="place-card__price-text">&#47;&nbsp;night</span>
-            </div>
-            <button className="place-card__bookmark-button button" type="button">
-              <svg className="place-card__bookmark-icon" width="18" height="19">
-                <use xlinkHref="#icon-bookmark"></use>
-              </svg>
-              <span className="visually-hidden">To bookmarks</span>
-            </button>
+const createOffers = (offers) => {
+  offers.map((offer, index) => (
+    <article className="cities__place-card place-card" key={index}>
+      <div className="place-card__mark">
+        <span>Premium</span>
+      </div>
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
+        </a>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;120</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <div className="place-card__rating rating">
-            <div className="place-card__stars rating__stars">
-              <span style={{width: `80%`}}></span>
-              <span className="visually-hidden">Rating</span>
-            </div>
-          </div>
-          <h2 className="place-card__name">
-            <a href="#">{offer}</a>
-          </h2>
-          <p className="place-card__type">Apartment</p>
+          <button className="place-card__bookmark-button button" type="button">
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">To bookmarks</span>
+          </button>
         </div>
-      </article>
-    </Fragment>
-  );
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style="width: 80%"></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">{offer.title}</a>
+        </h2>
+        <p className="place-card__type">Apartment</p>
+      </div>
+    </article>
+  ));
 };
 
 const Main = ({countOffers, offers}) => {
@@ -104,7 +102,7 @@ const Main = ({countOffers, offers}) => {
                 </ul>
               </form>
               <div className="cities__places-list places__list tabs__content">
-                {offers.map((offer, index) => <Offer key={index} offer={offer} />)}
+                {createOffers(offers)}
               </div>
             </section>
             <div className="cities__right-section">
@@ -118,6 +116,5 @@ const Main = ({countOffers, offers}) => {
 };
 
 Main.propTypes = types;
-Offer.propTypes = types.offer;
 
 export default Main;
